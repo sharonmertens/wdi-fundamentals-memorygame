668d408f636804cdfc87532faf910260cc1794f7
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2018-12-09 16:00:49 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,37 @@
 console.log("Up and running!");
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 	console.log("User flipped " + cardOne);
 	console.log("User flipped " + cardTwo);
+
 var cards = ["queen", "queen", "king", "king"]
+
 var cardsInPlay = []
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
+
 var cardOne = "queen";
 cardsInPlay.push("cardOne"); // adds cardOne to cardsInPlay
-console.log(cards);
+	console.log("User flipped queen");
+
 var cardTwo = "queen";
 cardsInPlay.push("cardTwo"); // adds cardTwo to cardsInPlay
-console.log(cardsInPlay);
+	console.log("User flipped queen");
 
 var flipCard = function (cardId) {
 	console.log("User flipped" + cards[cardId])
 }
+
+flipCard (0);
+flipCard (2);
+
+checkForMatch ();
